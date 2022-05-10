d8faa62bad67b78e04c94b15aff49d8e10b2c77e
HenHock <75622518+HenHock@users.noreply.github.com>
2022-05-10 14:50:03 +0300

    Scene customization

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -11,12 +11,25 @@ public class UIController : MonoBehaviour
     public PanelController endGamePanel;
     public PanelController updateCharacterPanel;
 
+
+    [SerializeField] private GameObject nextLevelButton;
+    [SerializeField] private GameObject previouslyLevelButton;
     [SerializeField] private int startMoney;
     [SerializeField] private Text textFieldMoney;
 
     private void Start()
     {
         changeMoney(startMoney);
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            nextLevelButton.SetActive(true);
+            previouslyLevelButton.SetActive(false);
+        }else if(SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+        {
+            nextLevelButton.SetActive(false);
+            previouslyLevelButton.SetActive(true);
+        }
 
         buyCharacterPanelController.Close();
         endGamePanel.Close();
