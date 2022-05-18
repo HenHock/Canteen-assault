748cc1266b07e828dde6bd267260a00438eda2e0
HenHock <75622518+HenHock@users.noreply.github.com>
2022-05-18 08:47:46 +0300

    Some character changes

--- Assets/Scripts/Game/Map/MapGenerator.cs
+++ Assets/Scripts/Game/Map/MapGenerator.cs
@@ -11,9 +11,9 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private GameObject characterSpawnPlacePrefab;
 
     public static int[,] titleArray;
-    public static int countR = 17;
-    public static int countC = 10;
-    /*
+    public static int countR = 14;
+    public static int countC = 15;
+    
     private void Start()
     {
         /*
@@ -22,27 +22,24 @@ public class MapGenerator : MonoBehaviour
          *-2- character who damage enemy
          *-3- enemy spawn possition
          *-4- finish 
-         *//*
-        
-        DataManager.titleArray = new int[,]
+        */
+
+        titleArray = new int[,]
         {
-            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1 },
-            {-1,-1,-1,-1,-1,-1,-1,-1,-3,-1 },
-            {-1,-1,-1,-1,-1,-1,-1,-1,-0,-1 },
-            {-1,-1,-1,-1,-1,-2,-0,-0,-0,-1 },
-            {-1,-1,-0,-0,-0,-0,-0,-1,-1,-1 },
-            {-1,-1,-0,-1,-1,-1,-1,-1,-1,-1 },
-            {-1,-2,-0,-1,-1,-1,-1,-1,-1,-1 },
-            {-1,-1,-0,-1,-1,-2,-1,-1,-1,-1 },
-            {-1,-1,-0,-1,-0,-0,-0,-1,-1,-1 },
-            {-1,-1,-0,-0,-0,-1,-0,-1,-1,-1 },
-            {-1,-1,-1,-1,-1,-1,-0,-2,-1,-1 },
-            {-1,-1,-1,-1,-1,-1,-0,-1,-1,-1 },
-            {-1,-1,-1,-1,-0,-0,-0,-1,-1,-1 },
-            {-1,-1,-1,-1,-0,-2,-0,-1,-1,-1 },
-            {-1,-0,-0,-0,-0,-0,-0,-1,-1,-1 },
-            {-1,-4,-1,-1,-1,-1,-1,-1,-1,-1 },
-            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1 }
+            {-1,-1,-1,-1,-1,-0,-0,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-0,-0,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-0,-0,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-0,-0,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-0,-0,-0,-0,-0, -0, -0, -0,-1 ,-1 },
+            {-1,-1,-1,-1,-1,-0,-0,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 },
+            {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1, -1, -1 ,-1 }
         };
 
         for (int i = countR-1; i >= 0; i--)
@@ -51,23 +48,23 @@ public class MapGenerator : MonoBehaviour
                 GameObject title = null;
                 /* 
                  * 1px = 155 unity unit;
-                 * V3(0,-0,-0) is the center of the screen, we find the top-left location to start drawing the map.
-                 *//*
+                 * V3(0,-1,-1) is the center of the screen, we find the top-left location to start drawing the map.
+                 */
                 Vector3 position = new Vector3(j- Screen.width / 2 / 155,-10, (i- Screen.height / 2 / 155)*(-1));
 
-                if (DataManager.titleArray[i, j] == 0)
+                if (titleArray[i, j] == 0)
                 {
                     title = Instantiate(groundPrefab);
                 }
-                else if (DataManager.titleArray[i, j] ==-2)
+                else if (titleArray[i, j] ==-2)
                     title = Instantiate(characterSpawnPlacePrefab);
-                else if (DataManager.titleArray[i, j] ==-3)
+                else if (titleArray[i, j] ==-3)
                 {
                     title = Instantiate(respawnPrefab);
-                    DataManager.titleArray[i, j] = 0;
+                    titleArray[i, j] = 0;
                 }
 
-                else if (DataManager.titleArray[i, j] ==-4)
+                else if (titleArray[i, j] ==-4)
                 {
                     title = Instantiate(finishPrefab);
                 }
@@ -80,5 +77,5 @@ public class MapGenerator : MonoBehaviour
                 }
             }
     
-    }*/
+    }
 }
