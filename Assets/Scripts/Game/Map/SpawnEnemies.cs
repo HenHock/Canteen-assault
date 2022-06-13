98111e8c6dacc0cccc7f715cef2922b4883f6631
DariaNecrasova <92520657+DariaNecrasova@users.noreply.github.com>
2022-06-13 10:54:18 +0300

    smrth to save 2

--- Assets/Scripts/Game/Map/SpawnEnemies.cs
+++ Assets/Scripts/Game/Map/SpawnEnemies.cs
@@ -72,8 +72,8 @@ public class SpawnEnemies : MonoBehaviour
     {
         for (int numberSquard = 0; numberSquard < enemyWavesList[numberWave].allSquardsList.Count; numberSquard++)
         {
-                SpawnGroupe(numberWave, numberSquard);
-                yield return new WaitForSeconds(timeSpawn);
+            SpawnGroupe(numberWave, numberSquard);
+            yield return new WaitForSeconds(timeSpawn);
         }
     }
     private void SpawnGroupe(int numberWave, int numberSquard)
